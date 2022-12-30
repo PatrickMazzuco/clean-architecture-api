8f7125d778be79d4fbffc0fcb211d9d79859b069
PatrickMazzuco <patrickmazzuco@gmail.com>
2022-12-30 20:17:28 -0300

    test: ensure CryptoAdapter returns the hashed password on success

--- src/infra/cryptography/crypto-adapter.service.spec.ts
+++ src/infra/cryptography/crypto-adapter.service.spec.ts
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import crypto from 'node:crypto';
 
 import { CryptoAdapter } from './crypto-adapter.service';
+
+jest.mock('node:crypto', () => ({
+  randomBytes: jest.fn().mockReturnValue('any_salt'),
+  pbkdf2Sync: jest.fn().mockReturnValue('any_hash')
+}));
 
 describe('Crypto Adapter', () => {
   it('should call node:crypto with correct value', async () => {
@@ -13,7 +19,7 @@ describe('Crypto Adapter', () => {
     const sut = new CryptoAdapter(config);
     const valueToEncrypt = 'any_value';
 
-    const encryptSpy = jest.spyOn(crypto, 'pbkdf2');
+    const encryptSpy = jest.spyOn(crypto, 'pbkdf2Sync');
 
     await sut.encrypt(valueToEncrypt);
 
@@ -22,8 +28,23 @@ describe('Crypto Adapter', () => {
       expect.any(String),
       config.iterations,
       config.keyLength,
-      config.digest,
-      expect.any(Function)
+      config.digest
     );
+  });
+
+  it('should return the hashed password on success', async () => {
+    const config: CryptoAdapter.Params = {
+      iterations: 10000,
+      keyLength: 64,
+      digest: 'sha512'
+    };
+
+    const sut = new CryptoAdapter(config);
+    const valueToEncrypt = 'any_value';
+
+    const hashedPassword = await sut.encrypt(valueToEncrypt);
+
+    expect(hashedPassword).toEqual('any_hash');
+    expect(hashedPassword).not.toEqual(valueToEncrypt);
   });
 });
