fac95c3fed6b00029c9d514a349b27e1ce9b4ccc
Patrick Mazzuco <patrickmazzuco@gmail.com>
2023-01-01 16:39:45 +0000

    feat: ensure requests return json as default content type

--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express';
 
 import { bodyparser } from './body-parser.middleware';
+import contentType from './content-type.middleware';
 import cors from './cors.middleware';
 
 const setupMiddlewares = (app: Express): void => {
   app.use(bodyparser);
+  app.use(contentType);
   app.use(cors);
 };
 
