206a898bb9638a92db2f302ca20b399215862878
Patrick Mazzuco <patrickmazzuco@gmail.com>
2023-01-01 16:27:40 +0000

    feat: ensure cors is enabled

--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express';
 
 import { bodyparser } from './body-parser.middleware';
+import cors from './cors.middleware';
 
 const setupMiddlewares = (app: Express): void => {
   app.use(bodyparser);
+  app.use(cors);
 };
 
 export default setupMiddlewares;
