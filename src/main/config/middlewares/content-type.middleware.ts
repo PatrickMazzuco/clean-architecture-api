2b59fa2284b8cfb5f90bc97db6432b906cba4513
Patrick Mazzuco <patrickmazzuco@gmail.com>
2023-01-01 16:42:40 +0000

    refactor: change to named exports

--- src/main/config/middlewares/content-type.middleware.ts
+++ src/main/config/middlewares/content-type.middleware.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
-const contentType = (req: Request, res: Response, next: NextFunction): void => {
+export const contentType = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
   res.type('json');
   next();
 };
-
-export default contentType;
--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -1,8 +1,8 @@
 import { Express } from 'express';
 
 import { bodyparser } from './body-parser.middleware';
-import contentType from './content-type.middleware';
-import cors from './cors.middleware';
+import { contentType } from './content-type.middleware';
+import { cors } from './cors.middleware';
 
 const setupMiddlewares = (app: Express): void => {
   app.use(bodyparser);
