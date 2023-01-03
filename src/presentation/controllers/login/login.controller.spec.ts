d3c22cf924ea83d9ffa40c5800cee78e5a26b19b
PatrickMazzuco <patrickmazzuco@gmail.com>
2023-01-03 22:22:22 +0000

    refactor: update sut factory

--- src/presentation/controllers/login/login.controller.spec.ts
+++ src/presentation/controllers/login/login.controller.spec.ts
@@ -2,13 +2,20 @@ import { LoginController } from './login.controller';
 import { MissingParamError } from '@/presentation/errors';
 import { HttpResponseFactory } from '@/presentation/helpers/http.helper';
 
-const makeSut = (): LoginController => {
-  return new LoginController();
+type SutTypes = {
+  sut: LoginController;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController();
+  return {
+    sut
+  };
 };
 
 describe('Login Controller', () => {
   it('should return 400 if no email is provided', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -22,7 +29,7 @@ describe('Login Controller', () => {
   });
 
   it('should return 400 if no password is provided', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: 'any_email@email.com'
