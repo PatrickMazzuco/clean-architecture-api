e7e72153c3e9322c6c3daae27563b498eaf82bea
PatrickMazzuco <patrickmazzuco@gmail.com>
2023-01-05 16:38:33 +0000

    refactor: create test factories

--- src/application/usecases/authentication/authentication.usecase.spec.ts
+++ src/application/usecases/authentication/authentication.usecase.spec.ts
@@ -1,31 +1,52 @@
 import { AuthenticationUseCase } from './authentication.usecase';
 import { FindAccountByEmailRepository } from '@/application/protocols/find-account-by-email.repository';
 import { Account } from '@/domain/entities';
+import { Authentication } from '@/domain/usecases/authentication.usecase';
+
+const mockAccount = (): Account => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@email.com',
+  password: 'valid_password'
+});
+
+const mockAuthData = (): Authentication.Params => {
+  const { email, password } = mockAccount();
+
+  return {
+    email,
+    password
+  };
+};
+
+type SutTypes = {
+  sut: AuthenticationUseCase;
+  findAccountByEmailRepositoryStub: FindAccountByEmailRepository;
+};
+
+const makeSut = (): SutTypes => {
+  class FindAccountByEmailRepositoryStub
+    implements FindAccountByEmailRepository
+  {
+    async findByEmail(email: string): Promise<Account | null> {
+      return await new Promise((resolve) => resolve(mockAccount()));
+    }
+  }
+
+  const findAccountByEmailRepositoryStub =
+    new FindAccountByEmailRepositoryStub();
+  const sut = new AuthenticationUseCase(findAccountByEmailRepositoryStub);
+
+  return {
+    sut,
+    findAccountByEmailRepositoryStub
+  };
+};
 
 describe('Authentication Usecase', () => {
   test('should call FindAccountByEmailRepository with correct email', async () => {
-    class FindAccountByEmailRepositoryStub
-      implements FindAccountByEmailRepository
-    {
-      async findByEmail(email: string): Promise<Account | null> {
-        return await new Promise((resolve) =>
-          resolve({
-            id: 'valid_id',
-            name: 'valid_name',
-            email: 'valid_email@email.com',
-            password: 'valid_password'
-          })
-        );
-      }
-    }
-
-    const findAccountByEmailRepositoryStub =
-      new FindAccountByEmailRepositoryStub();
-    const sut = new AuthenticationUseCase(findAccountByEmailRepositoryStub);
-    const authData = {
-      email: 'valid_email@email.com',
-      password: 'valid_password'
-    };
+    const { sut, findAccountByEmailRepositoryStub } = makeSut();
+    const authData = mockAuthData();
 
     const findAccountSpy = jest.spyOn(
       findAccountByEmailRepositoryStub,
