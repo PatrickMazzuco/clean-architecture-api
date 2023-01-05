4dbdae10ee04e4fb795cd3b9bc946ddcf38ec748
PatrickMazzuco <patrickmazzuco@gmail.com>
2023-01-05 22:30:06 +0000

    refactor: move bcrypt adapter

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { LogControllerDecorator } from '../../decorators/log.decorator';
 import { makeSignUpValidator } from './signup-validator';
 import { AddAccountUsecase } from '@/application/usecases/add-account/add-account.usecase';
-import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter.service';
+import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adapter.service';
 import { AccountMongoRepository } from '@/infra/db/mongodb/account-repository/account.repository';
 import { LogMongoRepository } from '@/infra/db/mongodb/log-repository/log.repository';
 import { SignUpController } from '@/presentation/controllers/signup/signup.controller';
