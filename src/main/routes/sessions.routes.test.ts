e47007a61e352e7de0fb98cdd249a8bcd124ab25
PatrickMazzuco <patrickmazzuco@gmail.com>
2023-01-06 00:53:17 +0000

    refactor: rename signup routes to sessions routes

---
+++ src/main/routes/sessions.routes.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest';
+
+import app from '../config/app';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongodb.helper';
+
+describe('Sessions Route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  describe('POST /signup', () => {
+    it('should return an account on success', async () => {
+      const accountData = {
+        name: 'Valid Name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      };
+
+      const response = await request(app)
+        .post('/api/signup')
+        .send(accountData)
+        .expect(200);
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          name: accountData.name,
+          email: accountData.email,
+          password: expect.any(String)
+        })
+      );
+    });
+  });
+});
