57c36f605de8a0c4dc6071393b56583e4858436d
Patrick Mazzuco <patrickmazzuco@gmail.com>
2023-01-18 21:40:32 +0000

    chore: remove uneeded packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   tsconfig: 'tsconfig.build.json',
   entry: ['src', '!src/**/*.spec.*', '!src/**/*.test.*'],
-  sourcemap: true,
   silent: true,
   clean: true,
   minify: true,
